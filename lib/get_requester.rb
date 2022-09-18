d2107c53e39020277ed1fa7b62ecd476e5a6d89b
Karu Samuel <karusamuelngugi@gmail.com>
2022-09-18 17:22:49 +0300

    complete Assignmnt

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,30 @@
 # Write your code here
+
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+
+class GetRequester
+    @url
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body()
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+
+    end
+
+    def  parse_json()
+        body =  get_response_body()
+
+        JSON.parse(body)
+
+    end
+
+
+
+end
